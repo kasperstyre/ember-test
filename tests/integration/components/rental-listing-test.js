07713943f04befb4d517ede8ed7fa196c1e1616a
Kasper Riis Pedersen <kaspermail1999@gmail.com>
2019-11-11 13:52:20 +0100

    Added map service

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -4,10 +4,17 @@ import { render, click } from '@ember/test-helpers';
 import EmberObject from '@ember/object';
 import hbs from 'htmlbars-inline-precompile';
 
+let StubMapsService = Service.extend({
+  getMapElement() {
+    return Promise.resolve(document.createElement('div'));
+  }
+});
+
 module('Integration | Component | rental-listing', function(hooks) {
     setupRenderingTest(hooks);
 
     hooks.beforeEach(function () {
+        this.owner.register('service:map-element', StubMapsService);
         this.rental = EmberObject.create({
             image: 'fake.png',
             title: 'test-title',
